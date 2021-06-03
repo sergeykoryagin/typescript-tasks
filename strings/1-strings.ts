0f42b2405c09daaa01c3fc209478fec500a96b84
sergeykoryagin <48332027+sergeykoryagin@users.noreply.github.com>
2021-06-03 15:55:22 +0300

    1-strings task

--- strings/1-strings.ts
+++ strings/1-strings.ts
@@ -1,10 +1,11 @@
+const uncapitalize = (str: string) => str.charAt(0).toLowerCase() + str.slice(1);
+
 const toSnakeCase = (str: string) => {
-    return str
-    .replace(/^[A-Z]/, letter => letter.toLowerCase())
-    .replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+    return uncapitalize(str)
+        .replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
 };
 
 
-let str = 'UpperCaseStringG';
+let str = 'UpperCaseWord';
 
 console.log(toSnakeCase(str));
