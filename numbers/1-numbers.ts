4a07a39859583f273a8875b8c121eeb0457fdee9
sergeykoryagin <48332027+sergeykoryagin@users.noreply.github.com>
2021-06-04 10:45:58 +0300

    1-numbers task

--- numbers/1-numbers.ts
+++ numbers/1-numbers.ts
@@ -8,16 +8,13 @@ const isSumDigPowEqualNum = (num: number) => {
 }
 
 const sumDigPow = (start: number, end: number): number[] => {
-    const result = []
-    for (let i = start; i < end; i++) {
-        if (isSumDigPowEqualNum(i)){
-            result.push(i);
-        }
-    }
-    return result;
+    return Array<number>(end - start).fill(0)
+        .map((num, ind) => num + ind + start)
+        .filter(num => isSumDigPowEqualNum(num));
 }
 
 
 console.log(sumDigPow(1, 10));
 console.log(sumDigPow(1, 100));
 console.log(sumDigPow(90, 100));
+console.log(sumDigPow(1, 1000000));
